5bbb5b41b7c443bb62a3a2ec400437102622d38a
Diego Dubon <diego.dubon@proximitycr.com>
2020-07-20 20:15:00 -0600

    added subject

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-const { send } = require('./handler.nodeMailer');
+const { send } = require('./handler.sendemail');
 
-router.post('/sendemail', send);
+/**
+ * @path /api/vi/sendemail
+ * @type application/json
+ * @method POST
+ * @param {Express.Request} req
+ * @param {object} req.body
+ * @param {string} req.body.message text used to send message
+ * @param {string} req.body.to email address destination which the message would be sent
+ * @param {Express.Response} res
+ */
+router.post('/api/v1/sendemail', send);
 
 module.exports = router;
